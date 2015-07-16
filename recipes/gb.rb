8ce81e7583005c38efd0a33f063f1f74a2650e49
Giles MM <giles.williams@gmail.com>
2015-07-16 14:04:00 +0000

    Added gb recipe

---
+++ recipes/gb.rb
@@ -0,0 +1,15 @@
+include_recipe 'osrm::install_git'
+
+Chef::Log.info("About to run osrm_map for GB")
+
+osrm_map 'great-britain' do
+  action :create_if_missing
+end
+
+Chef::Log.info("About to run osrm_routed for GB")
+
+osrm_routed 'great-britain' do
+  user   'osrm-routed'
+  listen '0.0.0.0'
+  port   5000
+end

d22142394118e959bf57ed921276a6991e87f07a
Giles Williams <giles@urbanmassage.com>
2015-07-16 17:26:44 +0000

    Make sure all osrm deps are installed

--- recipes/gb.rb
+++ recipes/gb.rb
@@ -1,4 +1,5 @@
 execute 'apt-get -y update'
+execute 'apt-get -y install git libboost-dev gcc g++ make cmake libstxxl-dev libxml2-dev libbz2-dev zlib1g-dev libzip-dev libboost-filesystem-dev libboost-thread-dev libboost-system-dev libboost-regex-dev libboost-program-options-dev libboost-iostreams-dev libgomp1 libpng-dev libprotoc7 libprotobuf-dev protobuf-compiler liblua5.1-0-dev libluabind-dev pkg-config libosmpbf-dev libtbb-dev libboost-test-dev'
 
 include_recipe 'osrm::install_git'
 

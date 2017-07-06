fd5e36da5602fa06d28eb8b4a2831fc05847f533
gileze33 <giles.williams@gmail.com>
2017-07-06 14:38:19 +0100

    update osrm github location

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-default['osrm']['repository'] = 'git://github.com/DennisOSRM/Project-OSRM.git'
+default['osrm']['repository'] = 'git://github.com/Project-OSRM/osrm-backend'
 default['osrm']['branch'] = 'v4.7.0'
 
 default['osrm']['target'] = '/opt/osrm'
